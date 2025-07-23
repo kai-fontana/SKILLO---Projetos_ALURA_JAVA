871b64b6eec36f8e4812615f8132a6b6a3dba852
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-23 08:39:59 -0300

    Add files via upload

---
+++ desafio-01.java
@@ -0,0 +1,8 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Kai!");
+    }    public static void (String[] args) {
+        System.out.println("Tudo bem com você?");
+    }
+
+}
