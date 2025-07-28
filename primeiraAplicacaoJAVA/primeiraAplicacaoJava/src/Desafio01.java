0e699f536b38dee22753969420c61eae460f941c
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-28 11:46:48 -0300

    Add files via upload

---
+++ primeiraAplicacaoJAVA/primeiraAplicacaoJava/src/Desafio01.java
@@ -0,0 +1,5 @@
+public class Desafio01 {
+    public static void main(String[] args) {
+        System.out.println("Desafio 1 alura");
+    }
+}
---
+++ primeiraAplicacaoJAVA/primeiraAplicacaoJava/src/Variavel.java
@@ -0,0 +1,5 @@
+public class Variavel {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo! ");
+    }
+}
