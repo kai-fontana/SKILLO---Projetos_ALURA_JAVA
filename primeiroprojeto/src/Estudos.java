761fdceb7888e02538e263e292dfd94803b1765b
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-28 11:53:53 -0300

    Delete primeiroprojeto directory

--- primeiroprojeto/src/Estudos.java
+++
@@ -1,7 +0,0 @@
-public class Estudos {
-    public static void main(String[] args) {
-        System.out.println("click no src + alt + insert + enter = criar uma classe");
-        System.out.println("psvm = public static void main");
-        System.out.println("sout = system.out.println");
-    }
-}
