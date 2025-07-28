3787a6a30d07d4afa81e745866ba89beb4a00195
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-28 08:41:43 -0300

    Add files via upload

---
+++ primeiroprojeto/src/Estudos.java
@@ -0,0 +1,5 @@
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("Finalmente entendi que cada classe é meio que como um arquivo e pra criar outra, preciso fazer isso no src.");
+    }
+}
