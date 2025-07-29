5fdf58ea8fac778d9d85beea7a8f5c5c6f4bfbcf
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-29 08:13:57 -0300

    Add files via upload

--- primeiraAplicacaoJAVA/primeiraAplicacaoJava/src/Main.java
+++ primeiraAplicacaoJAVA/primeiraAplicacaoJava/src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match!");
+        System.out.printf("Filme: Elementos");
 
-        System.out.printf("Hello and welcome!");
+        int anoDeLancamento = 2023;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluindoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
 
         }
 }
