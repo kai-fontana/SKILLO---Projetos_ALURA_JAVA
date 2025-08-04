69aa932634a2e73c8b737b5d6b589f53dbad3e2c
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-08-04 08:37:08 -0300

    Add files via upload

---
+++ exercicioJAVA02/exercicioCondicionais/src/exercicio01.java
@@ -0,0 +1,12 @@
+public class exercicio01 {
+    public static void main(String[] args) {
+        int numero = 19;
+        if ((numero % 2) == 0) {
+            System.out.println("Esse número é par");
+        } else if ((numero % 2) == 1){
+            System.out.println("Esse número é ímpar");
+        } else {
+            System.out.println("Esse número é inválido");
+        }
+    }
+}
---
+++ exercicioJAVA02/exercicioCondicionais/src/exercicio02.java
@@ -0,0 +1,8 @@
+public class exercicio02 {
+    public static void main(String[] args) {
+        double notaDoAluno01 = 8.0;
+        double notaDoAluno02 = 8.9;
+        double notaDoAluno03 = 8.5;
+        double mediaDaNota = notaDoAluno
+    }
+}
