d015bd5c4d77f11ae498a875565221108933d5ce
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-08-04 11:43:32 -0300

    Add files via upload

---
+++ orientacaoObjetos/orientacaoAObjetos/src/Principal.java
@@ -0,0 +1,27 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome =  "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        Filme meuFilme2 = new Filme();
+        meuFilme2.nome = "Procurando Nemo";
+        meuFilme2.anoDeLancamento = 2003;
+        meuFilme2.duracaoEmMinutos = 180;
+
+        System.out.println(meuFilme2.nome);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
+
+//Identação
+//CTRL + 1 = SELECIONAR TUDO
+// CTRL + ALT + I i
