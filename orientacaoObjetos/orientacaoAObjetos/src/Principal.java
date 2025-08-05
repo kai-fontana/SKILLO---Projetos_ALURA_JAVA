5cc49e380dc7ef579b02352ac5fce27fa3bce58b
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-08-05 11:33:03 -0300

    Add files via upload

--- orientacaoObjetos/orientacaoAObjetos/src/Principal.java
+++ orientacaoObjetos/orientacaoAObjetos/src/Principal.java
@@ -1,9 +1,11 @@
+import br.com.alura.screenmatch.modelo.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome =  "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         Filme meuFilme2 = new Filme();
         meuFilme2.nome = "Procurando Nemo";
@@ -16,9 +18,9 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.pegaMedia());
+
     }
 }
 
