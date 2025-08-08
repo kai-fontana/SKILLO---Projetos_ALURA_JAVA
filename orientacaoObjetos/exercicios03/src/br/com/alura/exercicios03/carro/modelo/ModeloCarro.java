2f6baa0e3ebb62845484baa811a01938c9af57c5
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-08-08 11:58:05 -0300

    Add files via upload

--- orientacaoObjetos/exercicios03/src/br/com/alura/exercicios03/carro/modelo/ModeloCarro.java
+++ orientacaoObjetos/exercicios03/src/br/com/alura/exercicios03/carro/modelo/ModeloCarro.java
@@ -1,6 +1,10 @@
 package br.com.alura.exercicios03.carro.modelo;
 
 public class ModeloCarro extends Carro {
-    ModeloCarro meuCarro = new ModeloCarro();
-    meuCarro.
+    public static void main(String[] args) {
+        ModeloCarro meuCarro = new ModeloCarro();
+        meuCarro.definirModelo("Onix");
+        meuCarro.definirPreco(90.000, 110.000, 120.000);
+        meuCarro.exibirInfo();
+    }
 }
