32e0b76e15c30f929e560279685b73a8f7b3372d
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-08-19 11:53:02 -0300

    Add files via upload

--- primeiraAplicacaoJAVA/primeiraAplicacaoJava/src/Main.java
+++ primeiraAplicacaoJAVA/primeiraAplicacaoJava/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Comentários
         System.out.println("Esse é o Screen Match!");
         System.out.printf("Filme: Elementos");
 
@@ -10,6 +11,21 @@ public class Main {
 
         double media = (9.8 + 6.3 + 8.0) /3;
         System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme:
+                Elementos
+                
+                Descrição:
+                Em uma cidade onde os habitantes de fogo, água, terra e ar convivem, uma
+                jovem mulher flamejante e um rapaz que vive seguindo o fluxo descobrem algo
+                surpreendente, porém elementar: o quanto eles têm em comum.
+                
+                Ano de lançamento: 
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
 
+        int classificacao = (int) (media/2);
+        System.out.println(classificacao);
         }
 }
